ef963fa7f8e0c779ba724ab86ae37096731a0fcf
Duda Becker <dudabeckerfoz@gmail.com>
2022-02-07 22:31:12 -0300

    Update ex2.php

--- ex2.php
+++ ex2.php
@@ -1,6 +1,10 @@
 <?php
-$num = 1;
 
-while($num <=100){
-    $num++;
+$soma = 0;
+
+for($num=1; $num <=100; $num++){
+    $soma += $num;
+    $media = $soma/100;
 }
+
+echo "A da soma dos números de 1 a 100 é ".$soma." e a média é ".$media;
